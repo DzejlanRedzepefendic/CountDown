0dec75ac12de6a39e3e5046a7315949e52942fa1
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-11-30 23:19:54 +0100

    Countdown Api

--- Projekt/server/controllers/countdown.js
+++ Projekt/server/controllers/countdown.js
@@ -2,14 +2,15 @@ const asyncWrapper = require('../middlewares/async')
 const {
   findAllCountdowns,
   createACountdown,
+  findACountdown,
+  findAndDeleteCountdown,
+  findAndUpdateCountdown,
 } = require('../services/countdown.js')
 
 const getAllCountdowns = asyncWrapper(async (req, res) => {
   const countdowns = await findAllCountdowns()
   res.status(200).json({ countdowns })
 })
-
-const getCountdown = asyncWrapper(async (req, res) => {})
 
 const createCountdown = asyncWrapper(async (req, res) => {
   const countdown = await req.body
@@ -23,14 +24,52 @@ const createCountdown = asyncWrapper(async (req, res) => {
     console.log(error)
     return res.status(500).json({ error })
   }
-  res.status(201).json({ createCount })
+  res
+    .status(201)
+    .json({ message: 'countdown created successfully', createCount })
 })
 
-const updateCountdown = asyncWrapper(async (req, res) => {})
+const getCountdown = asyncWrapper(async (req, res) => {
+  const { id: countdownID } = req.params
+  let countdown
+  try {
+    countdown = await findACountdown(countdownID)
+  } catch (errror) {
+    return res.status(404).json({ error: 'Invalid id params' })
+  }
+  res.status(200).json({ countdown })
+})
 
-const deleteCountdown = asyncWrapper(async (req, res) => {})
+const updateCountdown = asyncWrapper(async (req, res) => {
+  const { id: countdownID } = req.params
+  let countdown
+  try {
+    countdown = await findAndUpdateCountdown(countdownID, req.body)
+  } catch (error) {
+    console.log(error)
+    return res.status(404).json({ error: 'Invalid id params' })
+  }
+  res.status(200).json({ message: 'countdown updated successfully', countdown })
+})
+
+const deleteCountdown = asyncWrapper(async (req, res) => {
+  const { id: countdownID } = req.params
+  let countdown
+  try {
+    countdown = await findAndDeleteCountdown(countdownID)
+    if (!countdown) {
+      return res.status(400).json({ error: 'invalid id paramas' })
+    }
+  } catch (error) {
+    return res.status(404).json({ error: 'invalid id paramas', message: error })
+  }
+  res.status(200).json({ message: 'countdown deleted successfully' })
+})
 
 module.exports = {
   getAllCountdowns,
   createCountdown,
+  getCountdown,
+  deleteCountdown,
+  updateCountdown,
 }
--- Projekt/server/services/countdown.js
+++ Projekt/server/services/countdown.js
@@ -1,15 +1,35 @@
 const Countdown = require('../models/countdown')
-
+const {
+  findAll,
+  create,
+  findOne,
+  findAndUpdate,
+  findAndDelete,
+} = require('../utils/db/dbcrud')
 async function findAllCountdowns() {
-  const countdown = await Countdown.find({})
-  return countdown
+  return findAll(Countdown)
 }
 
 async function createACountdown(countdown) {
-  return Countdown.create(countdown)
+  return create(Countdown, countdown)
+}
+
+async function findACountdown(id) {
+  return findOne(Countdown, id)
+}
+
+async function findAndDeleteCountdown(id) {
+  return findAndDelete(Countdown, id)
+}
+
+async function findAndUpdateCountdown(id, updatedCountdown) {
+  return findAndUpdate(Countdown, id, updatedCountdown)
 }
 
 module.exports = {
   findAllCountdowns,
   createACountdown,
+  findACountdown,
+  findAndDeleteCountdown,
+  findAndUpdateCountdown,
 }
