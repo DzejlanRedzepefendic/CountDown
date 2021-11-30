47420e5b42b201a1d178603ca0cd84fadcea734c
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-11-30 23:20:39 +0100

    paramas routes

--- Projekt/server/routes/countdown.js
+++ Projekt/server/routes/countdown.js
@@ -3,6 +3,14 @@ const router = express.Router()
 const {
   getAllCountdowns,
   createCountdown,
+  getCountdown,
+  deleteCountdown,
+  updateCountdown,
 } = require('../controllers/countdown')
 router.route('/').get(getAllCountdowns).post(createCountdown)
+router
+  .route('/:id')
+  .get(getCountdown)
+  .delete(deleteCountdown)
+  .patch(updateCountdown)
 module.exports = router
