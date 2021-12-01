022e43dabb94612f92ba1fc7bf00546f48e0231a
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-12-01 17:01:44 +0100

    implementings cors

--- Projekt/server/index.js
+++ Projekt/server/index.js
@@ -8,7 +8,14 @@ const errorHandlerMiddleware = require('./middlewares/error-handler')
 const notFound = require('./middlewares/not-found')
 const connectDB = require('./db/connect')
 const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 const app = express()
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api/register', register)
 app.use('/api/login', login)
