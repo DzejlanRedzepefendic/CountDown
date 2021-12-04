009b4b5365030f853421ef1e1c9a9ec0e627fa05
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-12-04 16:29:33 +0100

    404 page

--- Projekt/client/src/components/NoMatchPage.jsx
+++ Projekt/client/src/components/NoMatchPage.jsx
@@ -3,21 +3,24 @@ import '../styles/404.css'
 const NoMatchPage = () => {
   return (
     <>
-      <h1>404</h1>
-      <div class='cloak__wrapper'>
-        <div class='cloak__container'>
-          <div class='cloak'></div>
+      <div className='content-wrap'>
+        <div className='not-found'></div>
+        <h1 className='header'>404</h1>
+        <div className='cloak__wrapper'>
+          <div className='cloak__container'>
+            <div className='cloak'></div>
+          </div>
         </div>
-      </div>
-      <div class='info'>
-        <h2>We can't find that page</h2>
-        <p>
-          We're fairly sure that page used to be here, but seems to have gone
-          missing. We do apologise on it's behalf.
-        </p>
-        <a href='#' target='_blank' rel='noreferrer noopener'>
-          Home
-        </a>
+        <div className='info'>
+          <h2>We can't find that page</h2>
+          <p>
+            We're fairly sure that page used to be here, but seems to have gone
+            missing. We do apologise on it's behalf.
+          </p>
+          <a href='#' target='_blank' rel='noreferrer noopener'>
+            Home
+          </a>
+        </div>
       </div>
     </>
   )
