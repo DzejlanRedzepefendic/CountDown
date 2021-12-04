38448a48a350c36a44194888e06e822bdf076c6a
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-12-04 16:31:06 +0100

    Countdown Component

--- Projekt/client/src/components/Countdown.jsx
+++ Projekt/client/src/components/Countdown.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
-
+import React, { useEffect, useState, useContext } from 'react'
+import { getAllCountdowns } from '../utils/axios/Countdown'
+import '../styles/Countdown.css'
+import { makeCountdown } from '../utils/MakeCountdown'
 const Countdown = () => {
-  return <div></div>
+  const [countDowns, setCountDowns] = useState([])
+  const [totalCD, setTotalCD] = useState(0)
+  useEffect(async () => {
+    const fetch = await getAllCountdowns()
+    console.log(fetch)
+    setCountDowns(fetch.data.countdowns)
+    setTotalCD(fetch.data.countdowns.length)
+  }, [])
+  return (
+    <div>
+      {!totalCD ? '' : <h1>Total countdowns in database:{totalCD}</h1>}
+      <div className='countdowns'>
+        {!countDowns ? (
+          <h1>No data</h1>
+        ) : (
+          countDowns.map((value, index) => {
+            return (
+              <div
+                key={value._id}
+                id={value._id}
+                className='card'
+                style={{
+                  backgroundImage: `url(${value.url})`,
+                  backgroundRepeat: 'no-repeat',
+                  backgroundPosition: 'center',
+                }}
+              >
+                <h1 id='title-text'>{value.title}</h1>
+                {makeCountdown(
+                  value.air_date.year,
+                  value.air_date.month,
+                  value.air_date.day,
+                  value.air_date.hour,
+                  value.air_date.minutes
+                )}
+              </div>
+            )
+          })
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Countdown
