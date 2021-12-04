dd98efd7d033d94aa4f5529756a9b8c8b884dbf5
DzejlanRedzepefendic <dzejlanven@icloud.com>
2021-12-04 16:31:26 +0100

    Search Component

--- Projekt/client/src/components/Search.jsx
+++ Projekt/client/src/components/Search.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import '../styles/Search.css'
 const Search = () => {
-  return <div></div>
+  return (
+    <div>
+      <div className='search'>
+        <div className='search-container'>
+          <input
+            type='text'
+            name='search'
+            placeholder='Search...'
+            className='search-input'
+          />
+          <a href='#' className='search-btn'>
+            <i className='fas fa-search'></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search
