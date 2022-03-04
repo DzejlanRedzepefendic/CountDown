e79ccb0ad83a9205b1829ba48c9ec02ac7f4e267
DzejlanRedzepefendic <dzejlanven@icloud.com>
2022-03-05 00:29:01 +0100

    cleaner code for Register component

--- Projekt/client/src/components/Register.jsx
+++ Projekt/client/src/components/Register.jsx
@@ -1,89 +1,94 @@
-import React, { useState } from 'react'
-import {Auth} from '../utils/axios/Auth'
-import '../styles/Register.css'
-import { useNavigate } from 'react-router-dom'
-import { apiPaths } from '../utils/axios/apiPaths'
+import React, { useState } from "react";
+import { Auth } from "../utils/axios/Auth";
+import { useNavigate } from "react-router-dom";
+import { apiPaths } from "../utils/axios/apiPaths";
+import "../styles/Register.css";
 
 const Register = () => {
   const [account, setAccount] = useState({
-    name: '',
-    email: '',
-    password: '',
-  })
-  const [password2, setPassword2] = useState('')
-  const navigate = useNavigate()
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const navigate = useNavigate();
 
-  const checkAccount = async (e) => {
-    e.preventDefault()
-    if (account.password === password2) {
-      return Auth(account, apiPaths.register).then((r)=> console.log(r))
+  const checkAccount = (e) => {
+    e.preventDefault();
+    if (account.password === account.password2) {
+      Auth(account, apiPaths.register).then((r) => console.log(r));
+      navigate("/");
     }
-    console.log('Invalid cresidentials')
-  }
+  };
+
+  const onChangeInput = (e) => {
+    setAccount({ ...account, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className='background'>
-      <div className='register-box'>
+    <div className="background">
+      <div className="register-box">
         <h2>Register</h2>
         <form>
-          <div className='user-box'>
+          <div className="user-box">
             <input
-              type='text'
+              type="text"
+              name="name"
               value={account.name}
-              onChange={(e) => setAccount({ ...account, name: e.target.value })}
+              onChange={onChangeInput}
             />
-            <label htmlFor=''>Full name</label>
+            <label htmlFor="">Full name</label>
           </div>
-          <div className='user-box'>
+          <div className="user-box">
             <input
-              type='email'
+              type="email"
+              name="email"
               value={account.email}
-              onChange={(e) =>
-                setAccount({ ...account, email: e.target.value })
-              }
+              onChange={onChangeInput}
             />
-            <label htmlFor=''>Email</label>
+            <label htmlFor="">Email</label>
           </div>
-          <div className='user-box'>
+          <div className="user-box">
             <input
-              type='password'
+              type="password"
+              name="password"
               value={account.password}
-              onChange={(e) =>
-                setAccount({ ...account, password: e.target.value })
-              }
+              onChange={onChangeInput}
             />
-            <label htmlFor=''>Password</label>
+            <label htmlFor="">Password</label>
           </div>
-          <div className='user-box'>
+          <div className="user-box">
             <input
-              type='password'
-              value={password2}
-              onChange={(e) => setPassword2(e.target.value)}
+              type="password"
+              name="password2"
+              value={account.password2}
+              onChange={onChangeInput}
             />
-            <label htmlFor=''>Re-enter password</label>
+            <label htmlFor="">Re-enter password</label>
           </div>
-          <div className='buttons-space'>
-            <a to='#'>
+          <div className="buttons-space">
+            <a to="#">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
               <p onClick={checkAccount}>Submit</p>
             </a>
-            <a to='#'>
+            <a to="#">
               <span></span>
               <span></span>
               <span></span>
               <span></span>
-              <p onClick={()=> navigate('/login')}>login</p>
+              <p onClick={() => navigate("/login")}>login</p>
             </a>
           </div>
         </form>
-        <a to='#'>
+        <a to="#">
           <p>Already have an account?</p>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
