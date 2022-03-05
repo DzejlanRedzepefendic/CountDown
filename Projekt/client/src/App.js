9e8a7927d547a9e05c7ab4354bd962ca3dad27ee
DzejlanRedzepefendic <dzejlanven@icloud.com>
2022-03-05 17:49:31 +0100

    Footer implemented

--- Projekt/client/src/App.js
+++ Projekt/client/src/App.js
@@ -3,9 +3,10 @@ import Navbar from "./components/layout/Navbar";
 import Login from "../src/components/auth/Login";
 import Register from "../src/components/auth/Register";
 import NoMatchPage from "./components/NoMatchPage";
-import Countdown from "./components/countdown/Countdown";
+import Countdown from "./components/countdown/CountDowns";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateCountDown from "./components/countdown/CreateCountDown";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/create" element={<CreateCountDown />} />
           <Route path="*" element={<NoMatchPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
--- Projekt/client/src/components/layout/Footer.jsx
+++ Projekt/client/src/components/layout/Footer.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import "../../styles/Footer.css";
 
 const Footer = () => {
-  return <div></div>
-}
+  return (
+    <footer>
+      <section class="footer-base">
+        <div class="footer-legal">
+          <div class="footer-legal-copyright">
+            Copyright © 2022 Vendor. All rights reserved.
+          </div>
+          <div class="footer-legal-links">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Use</a>
+            <a href="#">Sales and Refunds</a>
+            <a href="#">Legal</a>
+            <a href="#">Site Map</a>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
